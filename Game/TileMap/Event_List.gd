2c509060f625466ca7e8293c8f0c4ee8eb09b04e
João Marcos Mororo Costa <43460229+mororo250@users.noreply.github.com>
2018-10-23 09:56:26 -0300

    Agora os eventos não ficam em um loop infinito

--- Game/TileMap/Event_List.gd
+++ Game/TileMap/Event_List.gd
@@ -1,34 +1,35 @@
 extends CanvasLayer
 
+
+enum state {ACTIVE, INACTIVE}
+
 var questionsAsked = 0
-
-
-func shuffleList(list):
-    var shuffledList = [] 
-    var indexList = range(list.size())
-    for i in range(list.size()):
-        var x = randi()%indexList.size()
-        shuffledList.append(list[indexList[x]])
-        indexList.remove(x)
-    return shuffledList
+var eventState = ACTIVE
 
 
 func _on_Player_has_stopped(): 
-	var dialogue = get_parent().get_parent().get_node("HUD").get_node("Dialogue")
-	var event = get_parent().get_parent().get_node("HUD").get_node("Event_Cards")
-	var randomIndexArr = []
-	#Cria um array com todos os indices das perguntas
-	for i in range(dialogue.dialogData["pergunta"].size()):
-		randomIndexArr.push_back(i)
-	#randomiza os indices
-	randomIndexArr = shuffleList(randomIndexArr) #DEFEITO: Aparece mesma pergunta mais de uma vez
-	#mostra dialogo
-	dialogue.insert("pergunta", questionsAsked) #LEMBRETE: usar questionAsked apenas provisoriamente
-	dialogue.show_dialogue()
-	yield(dialogue, "dialogue_end")
-	#event.insert("positive", 0)
-	#event.show_event()
-	#yield(event, "event_end")
-	if questionsAsked < (dialogue.dialogData["pergunta"].size() - 1):
-		questionsAsked += 1 
+	if eventState == ACTIVE:
+		var dialogue = get_parent().get_parent().get_node("HUD").get_node("Dialogue")
+		var event = get_parent().get_parent().get_node("HUD").get_node("Event_Cards")
+		var t = Timer.new()
+		t.set_wait_time(0.25)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+		#mostra dialogo
+		dialogue.insert("pergunta", questionsAsked) #LEMBRETE: usar questionAsked apenas provisoriamente
+		dialogue.show_dialogue()
+		yield(dialogue, "dialogue_end")
+		t.start()
+		yield(t, "timeout")
+		#mostra carta de eventos
+		event.insert("positive", 0)
+		event.show_event()
+		yield(event, "event_end")
+		if questionsAsked < (dialogue.dialogData["pergunta"].size() - 1):
+			questionsAsked += 1 
+		eventState = INACTIVE
+		t.queue_free()
+	else:
+		eventState = ACTIVE
 
